7b612d52f4efcfe19c561dc1e928f9fc248715a4
szwagiers <mariusz_skowronski@wp.pl>
2020-10-13 13:38:26 +0200

    First lesson from Codemy.

---
+++ tutorial/firstLesson.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# first widget that always have to be. Creates root window
+root = Tk()
+
+# Label ?? Label widget
+myLabel = Label(root,text='Hello World')
+
+# Shoving it onto screen
+myLabel.pack()
+
+
+root.mainloop()
